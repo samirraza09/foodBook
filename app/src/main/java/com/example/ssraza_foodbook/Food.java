e78e89e28aa239cc1a12f4bf76885abe2991b565
Samir Raza Windows Laptop <samirraza09@gmail.com>
2022-09-19 16:38:40 -0600

    Added some text componenents for addShowFood activity

---
+++ app/src/main/java/com/example/ssraza_foodbook/Food.java
@@ -0,0 +1,78 @@
+package com.example.ssraza_foodbook;
+
+import java.util.Calendar;
+import java.util.Date;
+
+public class Food {
+
+    private String description;
+    private Date bestBeforeDate;
+    private String location;
+    private int count;
+    private int unitCost;
+
+    public Food(
+            String description,
+            Date bestBeforeDate,
+            String location,
+            int count,
+            int unitCost
+    ) {
+        this.description = description;
+        this.bestBeforeDate = bestBeforeDate;
+        this.location = location;
+        this.count = count;
+        this.unitCost = unitCost;
+    }
+
+    public Food(
+            String description,
+            int count
+    ) {
+        this.description = description;
+        this.bestBeforeDate = Calendar.getInstance().getTime();
+        this.location = "";
+        this.count = count;
+        this.unitCost = -1;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getBestBeforeDate() {
+        return bestBeforeDate;
+    }
+
+    public void setBestBeforeDate(Date bestBeforeDate) {
+        this.bestBeforeDate = bestBeforeDate;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getUnitCost() {
+        return unitCost;
+    }
+
+    public void setUnitCost(int unitCost) {
+        this.unitCost = unitCost;
+    }
+}
